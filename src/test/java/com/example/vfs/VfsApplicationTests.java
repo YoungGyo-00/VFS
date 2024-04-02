70f32e671ad482e9fd7a13fbfaba510a98a27a54
younggyo <lee4519698@hanmail.net>
2024-04-03 01:15:49 +0900

    init: 초기 환경 세팅

---
+++ src/test/java/com/example/vfs/VfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
