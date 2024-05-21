d2aa706122df7cb4ddbf4f0d61e104345e64dfa3
younggyo <lee4519698@hanmail.net>
2024-05-22 07:41:40 +0900

    feat : create ffmpeg config

---
+++ src/main/java/com/example/vfs/config/FFmpegConfig.java
@@ -0,0 +1,34 @@
+package com.example.vfs.config;
+
+import lombok.extern.slf4j.Slf4j;
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFprobe;
+import org.modelmapper.internal.util.Assert;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Slf4j
+@Configuration
+public class FFmpegConfig {
+    @Value("${ffmpeg.location}")
+    private String ffmpegLocation;
+    @Value("${ffprobe.location}")
+    private String ffprobeLocation;
+
+    @Bean(name = "ffmpeg")
+    public FFmpeg ffmpeg() throws IOException {
+        FFmpeg ffmpeg = new FFmpeg(ffmpegLocation);
+        Assert.isTrue(ffmpeg.isFFmpeg());
+        return ffmpeg;
+    }
+
+    @Bean(name = "ffprobe")
+    public FFprobe ffprobe() throws IOException {
+        FFprobe ffprobe = new FFprobe(ffprobeLocation);
+        Assert.isTrue(ffprobe.isFFprobe());
+        return ffprobe;
+    }
+}
